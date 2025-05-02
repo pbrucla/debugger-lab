cf252cbcb5035faf6bc46d3340919c69e5a5487f
en0mem <seekingblues@gmail.com>
2025-05-02 07:57:03 +0000

    Report errors more precisely

--- util.cpp
+++ util.cpp
@@ -1,13 +1,4 @@
 #include "util.hpp"
 
-#include <errno.h>
-
-#include <system_error>
-
 namespace util {
-void throw_errno() {
-    if (errno != 0) {
-        throw std::system_error(errno, std::generic_category());
-    }
-}
 }  // namespace util
