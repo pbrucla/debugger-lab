e99081b1b3f4c9fb891feb2b4be1e10d79efb7a7
en0mem <seekingblues@gmail.com>
2025-05-06 01:30:27 +0000

    Make ELF a class

--- elf.cpp
+++ elf.cpp
@@ -9,16 +9,43 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <optional>
+#include <string_view>
+#include <unordered_map>
+
 #include "util.hpp"
 
-void parse_elf(const char* filename) {
+ELF::ELF(const char* filename) { parse(filename); }
+
+ELF::~ELF() { munmap(m_file, m_filesize); }
+
+std::optional<uint64_t> ELF::lookup_sym(std::string_view name) const {
+    auto sym = m_syms.find(name);
+    if (sym == m_syms.end()) return {};
+    return sym->second;
+}
+
+Elf64_Shdr* ELF::find_section(const char* name) const {
+    for (size_t i = 0; i < m_shnum; ++i) {
+        auto* shdr = m_shdrs + i;
+        const char* sh_name = m_shstrtab + shdr->sh_name;
+        if (strcmp(name, sh_name) == 0) {
+            return shdr;
+        }
+    }
+    return nullptr;
+}
+
+void ELF::parse(const char* filename) {
     int fd = util::throw_errno(open(filename, O_RDONLY));
 
     struct stat sb;
     util::throw_errno(fstat(fd, &sb));
-    auto* file = static_cast<uint8_t*>(mmap(nullptr, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
-    util::throw_assert(file != MAP_FAILED, "mmap failed");
-    auto* ehdr = reinterpret_cast<Elf64_Ehdr*>(file);
+    m_filesize = sb.st_size;
+    m_file = static_cast<uint8_t*>(mmap(nullptr, m_filesize, PROT_READ, MAP_PRIVATE, fd, 0));
+    util::throw_assert(m_file != MAP_FAILED, "mmap failed");
+    auto* ehdr = reinterpret_cast<Elf64_Ehdr*>(m_file);
+    m_shnum = ehdr->e_shnum;
 
     util::throw_assert(memcmp(ehdr->e_ident, ELFMAG, SELFMAG) == 0, "not an ELF file");
     util::throw_assert(ehdr->e_machine == EM_X86_64, "unsupported machine");
@@ -27,39 +54,36 @@ void parse_elf(const char* filename) {
     util::throw_assert(ehdr->e_phentsize == sizeof(Elf64_Phdr), "wrong phdr size");
     util::throw_assert(ehdr->e_shentsize == sizeof(Elf64_Shdr), "wrong shdr size");
 
-    auto* shdrs = reinterpret_cast<Elf64_Shdr*>(file + ehdr->e_shoff);
-    auto* shdr_shstrtab = shdrs + ehdr->e_shstrndx;
+    m_shdrs = reinterpret_cast<Elf64_Shdr*>(m_file + ehdr->e_shoff);
+    auto* shdr_shstrtab = m_shdrs + ehdr->e_shstrndx;
     util::throw_assert(shdr_shstrtab->sh_type == SHT_STRTAB, "shstrtab is not a string table");
-    auto* shstrtab = reinterpret_cast<char*>(file + shdr_shstrtab->sh_offset);
+    m_shstrtab = reinterpret_cast<char*>(m_file + shdr_shstrtab->sh_offset);
 
-    Elf64_Off symtab_offset = 0;
-    Elf64_Off strtab_offset = 0;
-    size_t symtab_num = 0;
-    for (Elf64_Half i = 0; i < ehdr->e_shnum; ++i) {
-        auto* shdr = shdrs + i;
-        const char* name = shstrtab + shdr->sh_name;
-        printf("section %d: %s\n", i, name);
-        if (shdr->sh_type == SHT_SYMTAB) {
-            util::throw_assert(strcmp(name, ".symtab") == 0, "symbol table has unexpected section name");
-            util::throw_assert(shdr->sh_entsize == sizeof(Elf64_Sym), "symbol table has unexpected entry size");
-            symtab_offset = shdr->sh_offset;
-            symtab_num = shdr->sh_size / sizeof(Elf64_Sym);
-        } else if (shdr->sh_type == SHT_STRTAB && strcmp(name, ".strtab") == 0) {
-            strtab_offset = shdr->sh_offset;
+    auto collect_syms = [this](const char* symtab_name, const char* strtab_name) {
+        auto* symtab_shdr = find_section(symtab_name);
+        if (!symtab_shdr) {
+            printf("No %s section found\n", symtab_name);
+            return;
         }
-    }
+        util::throw_assert(symtab_shdr->sh_entsize == sizeof(Elf64_Sym), "symbol table has unexpected entry size");
+        util::throw_assert(symtab_shdr->sh_type == SHT_SYMTAB || symtab_shdr->sh_type == SHT_DYNSYM);
+        auto* strtab_shdr = find_section(strtab_name);
+        util::throw_assert(strtab_shdr, "symbol table has no corresponding string table");
+        util::throw_assert(strtab_shdr->sh_type == SHT_STRTAB);
 
-    util::throw_assert(symtab_offset != 0, "symbol table not found (handle gracefully later)");
-    util::throw_assert(strtab_offset != 0, "string table not found (handle gracefully later)");
-    auto* symtab = reinterpret_cast<Elf64_Sym*>(file + symtab_offset);
-    auto* strtab = reinterpret_cast<char*>(file + strtab_offset);
-    for (size_t i = 0; i < symtab_num; ++i) {
-        auto* sym = symtab + i;
-        if (ELF64_ST_TYPE(sym->st_info) == STT_FUNC && sym->st_value != 0) {
-            printf("symbol %s: %#lx\n", strtab + sym->st_name, sym->st_value);
+        auto* symtab = reinterpret_cast<Elf64_Sym*>(m_file + symtab_shdr->sh_offset);
+        auto* strtab = reinterpret_cast<char*>(m_file + strtab_shdr->sh_offset);
+        for (size_t i = 0; i < symtab_shdr->sh_size / sizeof(Elf64_Sym); ++i) {
+            auto* sym = symtab + i;
+            if (ELF64_ST_TYPE(sym->st_info) == STT_FUNC && sym->st_value != 0) {
+                m_syms.emplace(strtab + sym->st_name, sym->st_value);
+            }
         }
-    }
+    };
 
-    munmap(file, sb.st_size);
+    collect_syms(".symtab", ".strtab");
+    collect_syms(".dynsym", ".dynstr");
+    printf("%zu symbols loaded\n", m_syms.size());
+
     close(fd);
 }
