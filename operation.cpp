bde49b925ad3c462e910161ab61b813754c02250
kwong05 <kevin.g.wong.05@gmail.com>
2025-05-12 18:59:00 -0700

    rebase with main, comment out code for now

--- operation.cpp
+++ operation.cpp
@@ -7,14 +7,16 @@
 #include <optional>
 #include "operation.hpp"
 
-#include "elf.hpp"
+// #include "elf.hpp" TODO uncomment upon ELF merging into main
 #include "dbg.hpp"
 
+/* TODO uncomment upon ELF merging into main
 Operation::Operation(Tracee& tracee_arg, ELF& elf_arg)
 {
     tracee = &tracee_arg;
     elf = &elf_arg;
 }
+    */
 
 Operation::Operation(Tracee& tracee_arg)
 {
@@ -22,6 +24,7 @@ Operation::Operation(Tracee& tracee_arg)
     std::cout << "No ELF parsing will occur in this session. Refrain from using symbols in arguments.\n";
 }
 
+/* TODO uncomment upon ELF merging into main 
 long Operation::get_addr(std::string arg)
 {
     if (arg[0] == '*') // address
@@ -43,6 +46,7 @@ long Operation::get_addr(std::string arg)
         else return -1;
     }
 }
+    */
 
 std::vector<std::string> Operation::get_tokenize_command()
 {
@@ -80,6 +84,7 @@ int Operation::execute_command(std::vector<std::string> arguments)
     {
         if (command == "b" || command == "brk" || command == "break" || command == "breakpoint")
         {
+            /* TODO uncomment upon ELF merging into main
             // determine if we are working with an address or symbol
             std::string arg1 = arguments.at(1);
             long arg1_l = Operation::get_addr(arg1);
@@ -97,6 +102,8 @@ int Operation::execute_command(std::vector<std::string> arguments)
 
             tracee->insert_breakpoint(arg1_l); // set breakpoint
             std::cout << "Breakpoint added at" << arg1_l << "\n";
+
+            */
         }
         else if (command == "clr" || command == "clear")
         {
